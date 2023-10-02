d928152bcba2e15000a5abc47785f405247455fb
Oyewale <oyewalefavour424@gmail.com>
2023-10-02 19:47:32 +0100

    added categories

---
+++ src/Pages/HomeSections/HomeSection3.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const HomeSection3 = () => {
+  return (
+    <>
+     <section className='home-wrapper-2 py-5'>
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                    <div className="col-12">
+                        <div className="categories d-flex justify-content-between align-items-center">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section> 
+    </>
+  )
+}
+
+export default HomeSection3
